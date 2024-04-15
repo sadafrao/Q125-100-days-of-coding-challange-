1583fa5791bc715c357d58f87621b818ac76ae04
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:53:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Q125
+// make an object with multiple property and a method that using "this"
+const rectangle = {
+    length : 4,
+    width : 5,
+    calculateArea: function() {
+        return this.length * this.width;
+    }
+};
+console.log(rectangle.calculateArea());
